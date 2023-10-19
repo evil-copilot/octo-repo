16142030d81f5981b7a09d6e3c32e3c319204f54
hubwriter <hubwriter@github.com>
2023-10-19 14:20:54 +0100

    Create secret-sauce.js

---
+++ secret-sauce.js
@@ -0,0 +1,13 @@
+// include file system module
+var fs = require('fs');
+
+var data = "Hello there! I wrote this to a file."
+
+// write data to file sample.html
+fs.writeFile('sample.txt',data,
+    // callback function that is called after writing file is done
+    function(err) {
+        if (err) throw err;
+        // if no error
+        console.log("Data is written to file successfully.")
+});
